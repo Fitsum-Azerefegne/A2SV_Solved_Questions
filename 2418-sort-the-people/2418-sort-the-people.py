6b6915ee1b6eee81376b03e03c66644a6168cd6f
Fitsum Azerefegne <mininifinds@gmail.com>
2026-02-20 11:44:38 +0300

    Time: 748 ms (5.06%), Space: 19.7 MB (44.26%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -2,8 +2,12 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
        size = len(heights)
        for i in range(size):
+            swapped = False
             for j in range(size - i - 1):
                 if heights[j] < heights[j + 1]:
                     heights[j], heights[j + 1] = heights[j + 1], heights[j]
                     names[j], names[j + 1] = names[j + 1], names[j]
+                    swapped = True
+            if not swapped:
+                break
        return names 
