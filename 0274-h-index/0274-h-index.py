72644eba8ff45edf5c2a79c1fb4892426171ffd1
Fitsum Azerefegne <mininifinds@gmail.com>
2026-02-22 12:43:50 +0300

    Time: 0 ms (100%), Space: 19.5 MB (21.34%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,16 +1,10 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort()
-        maximum = 0
-        new_citation = []
-        for h in citations:
-            if h != 0 :
-                new_citation.append(h)
-        for i in range(len(new_citation)):
-            research = 0   
-            for j in range(len(new_citation)):
-                if new_citation[j] >= new_citation[i]:
-                    research += 1
-            h = min(research, new_citation[i])
-            maximum = max(maximum, h)  
-        return maximum
+        n = len(citations)
+        
+        for i in range(n):
+            h = n - i
+            if citations[i] >= h:
+                return h 
+        return 0
