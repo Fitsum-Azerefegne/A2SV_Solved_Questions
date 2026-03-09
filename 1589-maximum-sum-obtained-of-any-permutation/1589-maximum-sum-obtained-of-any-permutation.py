732413cb77028a8282e980e7c5c46a9374266464
Fitsum Azerefegne <mininifinds@gmail.com>
2026-03-09 19:36:50 +0300

    Time: 95 ms (49.21%), Space: 45.4 MB (60.24%) - LeetHub

---
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+        n = len(nums)
+        
+        freq = [0]*n
+        
+        for l,r in requests:
+            freq[l] += 1
+            if r+1 < n:
+                freq[r+1] -= 1
+        
+       
+        for i in range(1,n):
+            freq[i] += freq[i-1]
+        
+        nums.sort()
+        freq.sort()
+        
+        ans = 0
+        for i in range(n):
+            ans = (ans + nums[i]*freq[i]) % MOD
+     
+        return ans
