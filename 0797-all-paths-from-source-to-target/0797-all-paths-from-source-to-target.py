cf502d55345cc86463e4ec84ce176ce16a2990c1
Fitsum Azerefegne <mininifinds@gmail.com>
2026-05-08 19:44:34 +0300

    Time: 3 ms (93.58%), Space: 20.6 MB (51.45%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -2,8 +2,7 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         target = len(graph) - 1
-        result = []
-        
+        result = []    
         queue = deque([(0, [0])])  # (current_node, path)
 
         while queue:
